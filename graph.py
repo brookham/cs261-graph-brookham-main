d50086a3a3a1471646a6bb5443e5e0a4606f246c
brookham <bhamilton3264@gmail.com>
2025-03-06 20:56:08 -0800

    initial

---
+++ graph.py
@@ -0,0 +1,13 @@
+# Graph: An efficient graph.
+# A graph implementation that uses an adjacency list to represent vertices and edges.
+# Your implementation should pass the tests in test_graph.py.
+
+# Brook Hamilton
+# YOUR COLLABORATORS
+# TIME SPENT: 
+
+import functools
+
+class Graph:
+    def __init__(self):
+         self.data = {}
